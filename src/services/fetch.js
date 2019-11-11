31f5aa673e07af8b4af03c904aebeffbb5462819
ranckprogram <1474574022@qq.com>
2019-11-11 22:16:22 +0800

    eslint

---
+++ src/services/fetch.js
@@ -0,0 +1,11 @@
+
+
+function request () {
+  const options = {
+    includes: '',
+    headers: {
+      'Content-Type': '',
+    }
+  }
+}
+
--- src/views/User/detail.js
+++ src/views/User/detail.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 function Detail () {
+  console.log(1)
   return (
     <div>
       Details
